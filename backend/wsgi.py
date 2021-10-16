4063db89b6694f4330f581713719359f5878dd81
wajali7999 <wajali7999@gmail.com>
2021-10-17 03:35:14 +0500

    .

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
  
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.backend.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.backend.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
